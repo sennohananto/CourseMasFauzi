122f6544993bf2ae7ab0f980a0156d297828aa7f
Senno Hananto <sennohananto@gmail.com>
2020-09-17 20:23:32 +0700

    Komentar di class person list 1 sampai 5

--- app/src/main/java/com/noribaribaclub/coursemasfauzi/Person.java
+++ app/src/main/java/com/noribaribaclub/coursemasfauzi/Person.java
@@ -2,4 +2,12 @@ package com.noribaribaclub.coursemasfauzi;
 
 public class Person {
     //Ini komentar pertama di Class Person
+
+
+    //Ini adalah daftar nomor dari 1 sampai 5
+    // 1
+    // 2
+    // 3
+    // 4
+    // 5
 }
